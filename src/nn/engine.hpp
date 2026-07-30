b19321ddca81b441d74afdbda6081609ee605c36
Jacques Thurling <jthurling78@gmail.com>
2026-07-30 17:31:46 +0200

    More stuff

--- src/nn/engine.hpp
+++ src/nn/engine.hpp
@@ -1,14 +1,22 @@
 #include <functional>
+#include <memory>
 #include <string>
 #include <vector>
 
 struct Node {
   double data = 0.0;
   double grad = 0.0;
+  std::string op;
+  std::vector<std::shared_ptr<Node>> prev;
+  std::function<void()> backward = [] {};
 };
 
 namespace forgeml {
 // Stores scalar values and it's gradient
 class Value {
+  std::shared_ptr<Node> n_;
+
+public:
+  Value() : n_(std::make_shared<Node>()) {}
 };
 } // namespace forgeml
